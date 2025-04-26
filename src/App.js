ff2070e5440c2bfe181e9fcd9a7599e62e2ff68b
pantoine31 <antonhspap@icloud.com>
2025-04-26 22:04:32 +0300

    Fix: Switch to HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // άλλαξα BrowserRouter σε HashRouter
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,10 +7,8 @@ import BlogList from './components/BlogList';
 import BlogPost from './components/BlogPost';
 import Contact from './pages/Contact';
 import About from './pages/About';
-
 import Pro from './pages/Pro.js';
 import VisitorCounter from "./VisitorCounter";
-
 import posts from './components/posts.json'; // Save articles into posts.json
 
 function App() {
@@ -24,15 +22,13 @@ function App() {
             <Route path="/post/:id" element={<BlogPost posts={posts} />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
-
             <Route path="/proff" element={<Pro />} />
-
           </Routes>
         </main>
-        
+
         {/* Counter πάνω από το Footer */}
         <VisitorCounter />
-        
+
         <Footer className="footer" />
       </div>
     </Router>
